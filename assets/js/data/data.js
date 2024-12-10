ab7299a4dc9ad852d4d8ac74b2f757fea9871fee
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2024-12-10 16:44:22 +0100

    data for dynamically injecting the educations and projects

---
+++ assets/js/data/data.js
@@ -0,0 +1,60 @@
+const _allData = {
+    "education": [
+        {
+            "logo": "assets/media/howest-logo.svg",
+            "alt": "Logo Howest",
+            "date": "Sep 2024 - Present",
+            "institution": "Howest",
+            "type": "University of applied sciences",
+            "degree": "Bachelor CyberSecurity"
+        },
+        {
+            "logo": "assets/media/howest-logo.svg",
+            "alt": "Logo Howest",
+            "date": "Sep 2023 - Jun 2024",
+            "institution": "Howest",
+            "type": "University of applied sciences",
+            "degree": "Bachelor Applied Computer Science"
+        },
+        {
+            "logo": "assets/media/richtpunt_campus_eeklo-logo.svg",
+            "alt": "Logo Richtpunt Campus Eeklo",
+            "date": "Jan 2021 - Jun 2023",
+            "institution": "Richtpunt Campus Eeklo",
+            "degree": "Electricity - Electronics"
+        },
+        {
+            "logo": "assets/media/richtpunt_campus_eeklo-logo.svg",
+            "alt": "Logo Richtpunt Campus Eeklo",
+            "date": "Sep 2018 - Jan 2021",
+            "institution": "Richtpunt Campus Eeklo",
+            "degree": "Industrial Sciences"
+        },
+        {
+            "logo": "assets/media/ten-doorn-logo.svg",
+            "alt": "Logo College Ten Doorn",
+            "date": "Sep 2017 - Jun 2018",
+            "institution": "College Ten Doorn",
+            "degree": "Modern Sciences"
+        }
+    ],
+
+    "projects": [
+        {
+            "title": "Labyrinth",
+            "logo": "assets/media/labyrinth-logo.svg",
+            "alt": "Logo Labyrinth Project",
+            "github": "#",
+            "technologies": ["Java", "HTML", "CSS", "JavaScript"]
+        },
+        {
+            "title": "Magic The Gathering Deck Builder",
+            "logo": "assets/media/magic-the-gathering-project-logo.webp",
+            "alt": "Logo Magic The Gathering Deck Builder Project",
+            "github": "https://github.com/KjaniVE/MTGDeckBuilder",
+            "technologies": ["JavaScript"]
+        }
+    ]
+}
+
+export {_allData};
