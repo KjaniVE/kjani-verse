7cf70ebed7026f44bceff07873ac2902ef7f989f
Kjani Van Eenaeme <kjani.van.eenaeme@student.howest.be>
2024-12-10 18:06:12 +0100

    dynamically render in the projects from the datafile

---
+++ assets/js/projects.js
@@ -0,0 +1,35 @@
+import {_allData as DATA} from './data/data.js';
+
+init();
+
+function init() {
+    renderProjects(DATA.projects);
+}
+
+function renderProjects(data) {
+    const $canvas = document.querySelector(".proj-list");
+    $canvas.innerHTML = "";
+
+    for (const project of data) {
+        $canvas.insertAdjacentElement("beforeend", createProjectElement(project));
+    }
+
+}
+
+function createProjectElement(project) {
+
+    const $template = document.querySelector("#proj").content.firstElementChild.cloneNode(true);
+
+    $template.setAttribute("href", project.github);
+    $template.querySelector("h3").textContent = project.title;
+    $template.querySelector("img").setAttribute("src", project.logo);
+    $template.querySelector("img").setAttribute("alt", project.alt);
+
+    for (const technology of project.technologies) {
+        const $button = document.createElement("button");
+        $button.textContent = technology;
+        $template.querySelector(".proj-tech").insertAdjacentElement("beforeend", $button);
+    }
+
+    return $template
+}
